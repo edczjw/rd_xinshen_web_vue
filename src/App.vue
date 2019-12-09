3b1d75697c67416c17d51576c61bde53e9f284e8
zengjunwei1 <347350971@qq.com>
2019-12-09 19:23:41 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
   <div id="app">
     <router-view/>
+    <!-- 返回顶部 -->
+    <div id="dtop" class="gotop animated fadeInDown" title="返回顶部">
+     <div  @click="movetop()"> 顶部</div>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    
+    window.addEventListener("scroll", function(e) {
+      // 监听（绑定）滚轮滚动事件
+      var t = document.documentElement.scrollTop || document.body.scrollTop;
+      if (t > 310) {
+        document.getElementById("dtop").style.display = "block";
+      } else {
+        document.getElementById("dtop").style.display = "none";
+      }
+    });
+  },
+  methods: {
+    movetop() {
+      $("body,html").animate({ scrollTop: 0 }, 1000);
+    },
+    
+  },
+
 }
 </script>
 
@@ -20,5 +44,27 @@ body{
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+}
+/* 返回顶部 */
+.gotop {
+  position: fixed;
+  z-index: 9999;
+  right: 33px;
+  bottom: 35px;
+  display: none;
+  cursor: pointer;
+  width: 46px;
+  height: 46px;
+  line-height: 46px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: bolder;
+  color:rgb(22, 149, 223);
+  border-radius: 100%;
+  box-shadow: 0px 0px 0px 2px rgba(35, 170, 211, 0.61);
+  font-family: PingFangSC-Regular, sans-serif;
+}
+.gotop2:hover{
+  border: 1px solid rgb(184, 187, 184);
 }
 </style>
